2cf3e1cbd09f531fe42312750393bba577203944
dldlwjdwjdals <101513583+dldlwjdwjdals@users.noreply.github.com>
2022-10-29 23:15:34 +0900

    종목에 따른 시설 지도에 보여줌. 아직 완벽하지는 않은 듯.

---
+++ front/app/src/main/java/com/mju/exercise/Domain/MapDTO.java
@@ -0,0 +1,68 @@
+package com.mju.exercise.Domain;
+
+public class MapDTO {
+
+    String faciAddr1;
+    String faciAddr2;
+    String faciNm ;
+    String faciPointX ;
+    String faciPointY;
+    String ftypeNm;
+    String faciTel;
+
+    public String getFaciAddr2() {
+        return faciAddr2;
+    }
+
+    public void setFaciAddr2(String faciAddr2) {
+        this.faciAddr2 = faciAddr2;
+    }
+
+    public String getFaciTel() {
+        return faciTel;
+    }
+
+    public void setFaciTel(String faciTel) {
+        this.faciTel = faciTel;
+    }
+
+    public String getFaciAddr1() {
+        return faciAddr1;
+    }
+
+    public void setFaciAddr1(String faciAddr1) {
+        this.faciAddr1 = faciAddr1;
+    }
+
+    public String getFaciNm() {
+        return faciNm;
+    }
+
+    public void setFaciNm(String faciNm) {
+        this.faciNm = faciNm;
+    }
+
+    public String getFaciPointX() {
+        return faciPointX;
+    }
+
+    public void setFaciPointX(String faciPointX) {
+        this.faciPointX = faciPointX;
+    }
+
+    public String getFaciPointY() {
+        return faciPointY;
+    }
+
+    public void setFaciPointY(String faciPointY) {
+        this.faciPointY = faciPointY;
+    }
+
+    public String getFtypeNm() {
+        return ftypeNm;
+    }
+
+    public void setFtypeNm(String ftypeNm) {
+        this.ftypeNm = ftypeNm;
+    }
+}
