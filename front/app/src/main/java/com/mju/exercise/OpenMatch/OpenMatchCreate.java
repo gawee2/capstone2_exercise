a906b69da7babcee548e68f7ce8d4bcbce630532
duskite <duskite@github.com>
2022-11-15 12:10:20 +0900

    오픈매치 생성

--- front/app/src/main/java/com/mju/exercise/OpenMatch/OpenMatchCreate.java
+++ front/app/src/main/java/com/mju/exercise/OpenMatch/OpenMatchCreate.java
@@ -3,12 +3,15 @@ package com.mju.exercise.OpenMatch;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,17 +19,71 @@ import androidx.annotation.Nullable;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.textfield.TextInputEditText;
 import com.mju.exercise.R;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
+
 public class OpenMatchCreate extends BottomSheetDialogFragment {
+
+    Button btnCreate, btnClose;
+    TextInputEditText edtSubject, edtArticle;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.bottomsheet_open_match_create, container, false);
 
+        btnCreate = view.findViewById(R.id.btnCreate);
+        btnClose = view.findViewById(R.id.btnClose);
+        edtSubject = view.findViewById(R.id.edtSubject);
+        edtArticle = view.findViewById(R.id.edtArticle);
+
+        btnCreate.setOnClickListener(setOnClickListener);
+        btnClose.setOnClickListener(setOnClickListener);
+
+
         return view;
     }
+
+    private View.OnClickListener setOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.btnCreate:
+
+                    if(!createOpenMatch()){
+                        Toast.makeText(getContext(), "생성 실패", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    Toast.makeText(getContext(), "오픈매치 생성 완료", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.btnClose:
+                    dismiss();
+                    break;
+            }
+        }
+    };
+
+    private boolean createOpenMatch(){
+
+        edtSubject.getText().toString();
+        edtArticle.getText().toString();
+
+        return false;
+    }
+
+    private LocalDateTime nowTime(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return LocalDateTime.now(Clock.systemDefaultZone());
+        }
+        //오레오 미만 기기에서는 지원하지 않는 다고 함.
+        //나중에 추가 필요
+        return null;
+    }
+
 
     @NonNull
     @Override
