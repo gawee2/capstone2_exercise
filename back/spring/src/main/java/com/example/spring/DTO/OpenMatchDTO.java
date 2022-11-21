c52e747e97907fa2065df3b8fbb55b191b76c542
duskite <duskite@github.com>
2022-11-21 16:30:47 +0900

    날짜 통신 오류 수정중

--- back/spring/src/main/java/com/example/spring/DTO/OpenMatchDTO.java
+++ back/spring/src/main/java/com/example/spring/DTO/OpenMatchDTO.java
@@ -18,7 +18,7 @@ public class OpenMatchDTO {
 
     private String openUserId;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime openTime;
 
     private String subject;
@@ -27,7 +27,7 @@ public class OpenMatchDTO {
     private String sportType;
     private Integer personnel; //인원
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime playDateTime;
 
     private Double lat;
