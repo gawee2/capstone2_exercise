4c1c81b1f6cb30f172c14c1b0d04eaf4371fe95e
duskite <duskite@github.com>
2022-11-26 14:36:44 +0900

    필터링 다이얼로그 요일선택, 날짜 선택 값 넘겨주기까지 완료

--- front/app/src/main/java/com/mju/exercise/OpenMatch/FilteringDialog.java
+++ front/app/src/main/java/com/mju/exercise/OpenMatch/FilteringDialog.java
@@ -1,8 +1,10 @@
 package com.mju.exercise.OpenMatch;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -11,6 +13,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -21,13 +25,19 @@ import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.mju.exercise.R;
 import com.mju.exercise.StatusEnum.Status;
 import com.skydoves.expandablelayout.ExpandableLayout;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.zip.Inflater;
 
 public class FilteringDialog extends BottomSheetDialogFragment{
 
@@ -46,6 +56,9 @@ public class FilteringDialog extends BottomSheetDialogFragment{
             "가까운 거리순", "100m 이내", "500m 이내","1km 이내","3km 이내","3km 초과",
             "가까운 날짜순", "요일 선택", "특정 날짜 선택",
             "참여 가능만"
+    };
+    private String[] favDayStr = {
+            "월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"
     };
     private HashMap<String, Integer> chipMap = new HashMap<>();
 
@@ -179,15 +192,73 @@ public class FilteringDialog extends BottomSheetDialogFragment{
             case CHIP_INT +7:
                 filterTypeDay = Status.FilterTypeDay.DAY_FAVDAY;
                 //다이얼로그 띄워서 선호요일 고르도록
+                LayoutInflater inflater = getLayoutInflater();
+                View view = inflater.inflate(R.layout.dialog_favday_select, null);
+                RadioGroup radioGroup = (RadioGroup) view.findViewById(R.id.rdgFavDay);
+                new AlertDialog.Builder(getContext())
+                        .setTitle("요일 선택")
+                        .setView(view)
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                favRdbtnMapping(radioGroup.getCheckedRadioButtonId());
+                            }
+                        })
+                        .show();
                 break;
             case CHIP_INT +8:
                 filterTypeDay = Status.FilterTypeDay.DAY_PICK;
                 //다이얼로그 띄워서 특정날짜 고르도록
+                MaterialDatePicker materialDatePicker;
+                materialDatePicker = MaterialDatePicker.Builder.datePicker()
+                        .setTitleText("날짜를 선택")
+                        .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                        .build();
+                materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+                    @Override
+                    public void onPositiveButtonClick(Long selection) {
+                        Log.d("필터칩", "날짜:" + selection.toString());
+                        Date date = new Date(selection);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            mPickDay = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+                            Log.d("필터칩", "날짜:" + mPickDay.toString());
+                        }
+                    }
+                });
+
+                materialDatePicker.show(getChildFragmentManager(), "date");
                 break;
 
             //인원 필터
             case CHIP_INT +9:
                 filterTypeJoin = Status.FilterTypeJoin.JOIN_CAN;
+                break;
+        }
+    }
+
+    private void favRdbtnMapping(int id){
+        Log.d("필터칩", "라디오: " + String.valueOf(id));
+        switch (id){
+            case R.id.rdbtnMon:
+                mFavDay = Status.FavDayType.MON;
+                break;
+            case R.id.rdbtnTue:
+                mFavDay = Status.FavDayType.TUE;
+                break;
+            case R.id.rdbtnWed:
+                mFavDay = Status.FavDayType.WED;
+                break;
+            case R.id.rdbtnThu:
+                mFavDay = Status.FavDayType.THU;
+                break;
+            case R.id.rdbtnFri:
+                mFavDay = Status.FavDayType.FRI;
+                break;
+            case R.id.rdbtnSat:
+                mFavDay = Status.FavDayType.SAT;
+                break;
+            case R.id.rdbtnSun:
+                mFavDay = Status.FavDayType.SUN;
                 break;
         }
     }
